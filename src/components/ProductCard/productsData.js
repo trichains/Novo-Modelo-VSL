8ea13c7083fe3e5a8514edeeba285b5928fe373b
Cristhian Almeida <3correntes@gmail.com>
2024-05-13 21:10:58 -0400

    add: productsData

---
+++ src/components/ProductCard/productsData.js
@@ -0,0 +1,42 @@
+export const productsData = [
+  // 1 kit
+  {
+    title: 'Try One',
+    supply: '30 Day Supply',
+    price: '59',
+    oldPrice: '118',
+    shipping: '+ Shipping',
+    savings: '59',
+    bonus: '', // no bonuses
+    buttonText: 'BUY NOW!',
+    imageUrl: '/assets/products/1kit.png',
+    checkoutUrl: 'https://www.digistore24.com/product/546334' // 1 bottle - $59
+  },
+  // 6 kits
+  {
+    title: 'Best Value',
+    supply: '180 Day Supply',
+    price: '39',
+    oldPrice: '588',
+    shipping: '+ Free Us Shipping',
+    savings: '247',
+    bonus: '', // no bonuses
+    buttonText: 'BUY NOW!',
+    imageUrl: '/assets/products/6kits.png',
+    isFeatured: true, // A borda azul será exibida para destacar esse card do produto
+    checkoutUrl: 'https://www.digistore24.com/product/544946' // 6 bottles - $247
+  },
+  // 3 kits
+  {
+    title: 'Most Popular',
+    supply: '90 Day Supply',
+    price: '49',
+    oldPrice: '354',
+    shipping: '+ Free Us Shipping',
+    savings: '147',
+    bonus: '', // no bonuses
+    buttonText: 'BUY NOW!',
+    imageUrl: '/assets/products/3kits.png',
+    checkoutUrl: 'https://www.digistore24.com/product/544945' // 3 bottles - $147
+  }
+];
